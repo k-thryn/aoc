ff26317dbee04132fef6f653a11011f06423d028
k-thryn <kmstavish26@gmail.com>
2018-12-10 16:08:45 -0500

    day 3 part 2

--- src/day3/SolutionPt1&2.kt
+++ src/day3/SolutionPt1&2.kt
@@ -1,7 +1,7 @@
 package day3
 
 fun main(vararg args: String) {
-    var lines = ArrayList<String>()
+    val lines = ArrayList<String>()
     var line = readLine()
 
     while (line != null && !line.isEmpty()) {
@@ -13,7 +13,7 @@ fun main(vararg args: String) {
 }
 
 fun overLappingInches(lines: List<String>) {
-    var canvas = Canvas()
+    val canvas = Canvas()
     for (line in lines) {
         val splitLine = line.split("@|:".toRegex())
         val leftTop = splitLine[1].split(",")
@@ -25,21 +25,26 @@ fun overLappingInches(lines: List<String>) {
         val width = widthHeight[0].trim().toInt()
         val height = widthHeight[1].trim().toInt()
 
-        canvas.addHit(fromLeft, fromTop, width, height)
+        canvas.addHit(id, fromLeft, fromTop, width, height)
     }
 
+
     println(canvas.multipleHits.size)
+    println(canvas.findCleanRectangle())
 }
 
 // Pair<x, y>
 class Canvas {
     var oneHit = HashSet<Pair<Int, Int>>()
     var multipleHits = HashSet<Pair<Int, Int>>()
+    var rectangles = HashMap<Int, HashSet<Pair<Int, Int>>>() // <id, rectangle>
 
-    fun addHit(fromLeft: Int, fromTop: Int, width: Int, height: Int) {
+    fun addHit(id: Int, fromLeft: Int, fromTop: Int, width: Int, height: Int) {
+        var currentRectangle = HashSet<Pair<Int, Int>>()
         for (x in fromTop until fromTop + height) {
             for (y in fromLeft until fromLeft + width) {
                 val pair = Pair(x, y)
+                currentRectangle.add(pair)
                 if (!multipleHits.contains(pair)) {
                     if (!oneHit.contains(pair)) {
                         oneHit.add(pair)
@@ -50,5 +55,24 @@ class Canvas {
                 }
             }
         }
+        rectangles[id] = currentRectangle
+    }
+
+    fun findCleanRectangle(): Int {
+        var isClean = true
+        for ((id, rectangle) in rectangles) {
+            for (point in rectangle) {
+                if (multipleHits.contains(point)) {
+                    isClean = false
+                    break
+                }
+            }
+            if (isClean) {
+                return id
+            } else {
+                isClean = true
+            }
+        }
+        return -1 // not found
     }
 }
